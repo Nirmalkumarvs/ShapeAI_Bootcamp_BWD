00ec43af7783064e3777797e4a138899c6966034
Nirmalkumarvs <84463911+Nirmalkumarvs@users.noreply.github.com>
2021-06-26 17:11:31 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>Abasic web dev React Js Bootcamp</p>
+    </div>
+  );
+};
+
+export default Info;
